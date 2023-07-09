0ec05498cb1537b5540203f9b5b76bbc98c03133
vicek83 <vicek11@gmail.com>
2023-07-10 00:19:01 +0200

    Układ treści w sekcji blog

--- src/blog.jsx
+++ src/blog.jsx
@@ -13,10 +13,10 @@ const Blog = () => {
 
     async function getBlog() {
         try {
-            const { data: Blog, error} = await supabase
+            const {data: Blog, error} = await supabase
                 .from('Blog')
                 .select('*')
-                .order('id', { ascending: false })
+                .order('id', {ascending: false})
 
             if (error) throw error;
             if (Blog !== null) {
@@ -34,18 +34,23 @@ const Blog = () => {
             <div className="container mx-auto">
                 <div
                     className="mt-2 shadow-lg py-4 px-8 bg-gradient-to-b from from-gray-100 to-gray-200 rounded ring ring-gray-300 ring-offset-2">
-                    {blogNotes.map(note =>
-                        <li key={note.id}>
-                            <h3 className="font-black">{note.created_at}</h3>
-                            <h1 className="font-['courgette'] text-3xl">{note.title}</h1>
-                            <p className="">{note.description}</p>
-                            <ReactPlayer className="mx-auto" url={note.video} controls />
-                            </li>)}
-                        </div>
-                        </div>
+                    <div className="flex flex-wrap">
+                        {blogNotes.map((note => (
 
-                        </>
-                        )
-                    }
+                            <div key={note.id} className="w-1/3 p-4">
+                                <h3 className="font-black">{note.created_at}</h3>
+                                <h1 className="font-['courgette'] text-3xl">{note.title}</h1>
+                                <p className="">{note.description}</p>
+                                <img src={note.image} alt="Blog Image"/>
 
-                    export default Blog
+                            </div>
+                        )))}
+                    </div>
+                </div>
+            </div>
+
+        </>
+    );
+}
+
+export default Blog
